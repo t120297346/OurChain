3b014290163f2adebe22d0978f3ff129d4fdea17
leon123858 <a0970785699@gmail.com>
2024-01-22 20:39:38 +0000

    update

--- example/aid01.cpp
+++ example/aid01.cpp
@@ -29,98 +29,210 @@ only use password can not used in prod, but it's ok in this example
 
 using json = nlohmann::json;
 
-enum Command {
+enum Command
+{
     login,
     verify,
-    registerNewUser
+    registerNewUser,
+    getCoins,
+    setCoin,
+    removeCoin,
 };
 
-static std::unordered_map<std::string,Command> const string2Command = { {"login",Command::login}, {"verify",Command::verify}, {"registerNewUser",Command::registerNewUser} };
+static std::unordered_map<std::string, Command> const string2Command = {{"login", Command::login}, {"verify", Command::verify}, {"registerNewUser", Command::registerNewUser}, {"getCoins", Command::getCoins}, {"setCoin", Command::setCoin}, {"removeCoin", Command::removeCoin}};
 
-extern "C" int contract_main(int argc, char **argv) {
-  // init state
-  if(!state_exist()){
-    json j = json::array();
-    state_write(j);
-  }
-  // execute command
-  if (argc == 1) {
-      std::cerr << "argc error" << std::endl;
-      return 0;
-  }
-  std::string command = argv[1];
-  auto eCommand = string2Command.find(command);
-  if (eCommand == string2Command.end()) {
-      std::cerr << "command error" << std::endl;
-      return 0;
-  }
-  switch (eCommand->second)
-  {
-  case Command::login:
-    if(check_runtime_can_write_db()){
-        return 0;
-    }
+extern "C" int contract_main(int argc, char **argv)
+{
+    // init state
+    if (!state_exist())
     {
-        std::string name = argv[2];
-        std::string password = argv[3];
-        json j = state_read();
-        for (auto &i : j) {
-            if (i["name"] == name && i["password"] == password) {
-                json tmp = json::object();
-                tmp["isExist"] = true;
-                tmp["aid"] = i["aid"];
-                tmp["name"] = i["name"];
-                state_write(tmp);
-                return 0;
-            }
-        }
-        json tmp = json::object();
-        tmp["isExist"] = false;
-        state_write(tmp);
-        return 0;
-    }
-  case Command::verify:
-    if(check_runtime_can_write_db()){
-        return 0;
-    }
-    {
-        std::string aid = argv[2];
-        std::string password = argv[3];
-        json j = state_read();
-        for (auto &i : j) {
-            if (i["aid"] == aid) {
-                json tmp = json::object();
-                tmp["isExist"] = true;
-                if (i["password"] == password) {
-                    tmp["result"] = true;
-                } else {
-                    tmp["result"] = false;
-                }
-                state_write(tmp);
-                return 0;
-            }
-        }
-        json tmp = json::object();
-        tmp["isExist"] = false;
-        state_write(tmp);
-        return 0;
-    }
-  case Command::registerNewUser:
-    if(!check_runtime_can_write_db()){
-      return 0;
-    }
-    {
-        std::string name = argv[2];
-        std::string password = argv[3];
-        json j = state_read();
-        json tmp;
-        tmp["name"] = name;
-        tmp["password"] = password;
-        tmp["aid"] = boost::uuids::to_string(boost::uuids::random_generator()());
-        j.push_back(tmp);
+        json j = json::array();
         state_write(j);
     }
-    break;
-  }
-  return 0;
+    // execute command
+    if (argc == 1)
+    {
+        std::cerr << "argc error" << std::endl;
+        return 0;
+    }
+    std::string command = argv[1];
+    auto eCommand = string2Command.find(command);
+    if (eCommand == string2Command.end())
+    {
+        std::cerr << "command error" << std::endl;
+        return 0;
+    }
+    switch (eCommand->second)
+    {
+    case Command::login:
+        if (check_runtime_can_write_db())
+        {
+            return 0;
+        }
+        {
+            std::string name = argv[2];
+            std::string password = argv[3];
+            json j = state_read();
+            for (auto &i : j)
+            {
+                if (i["name"] == name && i["password"] == password)
+                {
+                    json tmp = json::object();
+                    tmp["isExist"] = true;
+                    tmp["aid"] = i["aid"];
+                    tmp["name"] = i["name"];
+                    state_write(tmp);
+                    return 0;
+                }
+            }
+            json tmp = json::object();
+            tmp["isExist"] = false;
+            state_write(tmp);
+            return 0;
+        }
+    case Command::verify:
+        if (check_runtime_can_write_db())
+        {
+            return 0;
+        }
+        {
+            std::string aid = argv[2];
+            std::string password = argv[3];
+            json j = state_read();
+            for (auto &i : j)
+            {
+                if (i["aid"] == aid)
+                {
+                    json tmp = json::object();
+                    tmp["isExist"] = true;
+                    if (i["password"] == password)
+                    {
+                        tmp["result"] = true;
+                    }
+                    else
+                    {
+                        tmp["result"] = false;
+                    }
+                    state_write(tmp);
+                    return 0;
+                }
+            }
+            json tmp = json::object();
+            tmp["isExist"] = false;
+            state_write(tmp);
+            return 0;
+        }
+    case Command::registerNewUser:
+        if (!check_runtime_can_write_db())
+        {
+            return 0;
+        }
+        {
+            std::string name = argv[2];
+            std::string password = argv[3];
+            json j = state_read();
+            json tmp;
+            tmp["name"] = name;
+            tmp["password"] = password;
+            tmp["aid"] = boost::uuids::to_string(boost::uuids::random_generator()());
+            j.push_back(tmp);
+            state_write(j);
+        }
+        break;
+    case Command::getCoins:
+        if (check_runtime_can_write_db())
+        {
+            return 0;
+        }
+        {
+            std::string aid = argv[2];
+            json j = state_read();
+            for (auto &i : j)
+            {
+                if (i["aid"] == aid)
+                {
+                    json tmp = json::object();
+                    tmp["isExist"] = true;
+                    // check if coins exist
+                    if (i.find("coins") != i.end())
+                    {
+                        tmp["coins"] = i["coins"];
+                    }
+                    else
+                    {
+                        tmp["coins"] = json::array();
+                    }
+                    state_write(tmp);
+                    return 0;
+                }
+            }
+            json tmp = json::object();
+            tmp["isExist"] = false;
+            state_write(tmp);
+            break;
+        }
+    case Command::setCoin:
+        if (!check_runtime_can_write_db())
+        {
+            return 0;
+        }
+        {
+            std::string aid = argv[2];
+            std::string coinAddress = argv[3];
+            json j = state_read();
+            for (auto &i : j)
+            {
+                if (i["aid"] == aid)
+                {
+                    // check if coins exist
+                    if (i.find("coins") != i.end())
+                    {
+                        i["coins"].push_back(coinAddress);
+                    }
+                    else
+                    {
+                        i["coins"] = json::array();
+                        i["coins"].push_back(coinAddress);
+                    }
+                    state_write(j);
+                    return 0;
+                }
+            }
+            break;
+        }
+    case Command::removeCoin:
+        if (!check_runtime_can_write_db())
+        {
+            return 0;
+        }
+        {
+            std::string aid = argv[2];
+            std::string coinAddress = argv[3];
+            json j = state_read();
+            for (auto &i : j)
+            {
+                if (i["aid"] == aid)
+                {
+                    // check if coins exist
+                    if (i.find("coins") != i.end())
+                    {
+                        auto tmp = json::array();
+                        for (auto &coin : i["coins"])
+                        {
+                            if (coin != coinAddress)
+                            {
+                                tmp.push_back(coin);
+                            }
+                        }
+                        i["coins"] = tmp;
+                        state_write(j);
+                        return 0;
+                    }
+                    break;
+                }
+            }
+            break;
+        }
+    }
+    return 0;
 }
--- example/orc20.cpp
+++ example/orc20.cpp
@@ -33,163 +33,191 @@ approve, transferFrom, allowance 用來花費別人的 TOKEN
 
 using json = nlohmann::json;
 
-enum Command {
-    totalSupply,
-    balanceOf,
-    transfer,
-    allowance,
-    approve,
-    transferFrom
+enum Command
+{
+  totalSupply,
+  balanceOf,
+  transfer,
+  allowance,
+  approve,
+  transferFrom
 };
 
-static std::unordered_map<std::string,Command> const string2Command = { {"totalSupply",Command::totalSupply}, {"balanceOf",Command::balanceOf}, {"transfer",Command::transfer}, {"allowance",Command::allowance}, {"approve",Command::approve}, {"transferFrom",Command::transferFrom} };
+static std::unordered_map<std::string, Command> const string2Command = {{"totalSupply", Command::totalSupply}, {"balanceOf", Command::balanceOf}, {"transfer", Command::transfer}, {"allowance", Command::allowance}, {"approve", Command::approve}, {"transferFrom", Command::transferFrom}};
 static std::string aidContractAddress = "f40ee8930eb5c30211e3691f37576c259121d673c1b55bc97e81ec518048c6da";
 
-char* getDynamicString(const char* str){
-    char* ret = (char*)malloc(sizeof(char) * strlen(str));
-    strcpy(ret, str);
-    return ret;
+char *getDynamicString(const char *str)
+{
+  char *ret = (char *)malloc(sizeof(char) * strlen(str));
+  strcpy(ret, str);
+  return ret;
 }
 
-void removeDynamicStrings(char** argv){
-    for(int i=0;i<4;i++){
-        delete[] argv[i];
-    }
-    delete[] argv;
+void removeDynamicStrings(char **argv)
+{
+  for (int i = 0; i < 4; i++)
+  {
+    delete[] argv[i];
+  }
+  delete[] argv;
 }
 
-bool verifyUser(std::string aid, std::string password){
-    // check if aid is valid
-    char **subArgv = (char **)malloc(sizeof(char *) * 4);
-    // put aidContractAddress into argv[0]
-    subArgv[0] = getDynamicString(aidContractAddress.c_str());
-    subArgv[1] = getDynamicString("verify");
-    subArgv[2] = getDynamicString(aid.c_str());
-    subArgv[3] = getDynamicString(password.c_str());
-    int ret = call_contract(aidContractAddress.c_str(), 4, subArgv);
-    removeDynamicStrings(subArgv);
-    if(ret != 0){
-        err_printf("aid contract exe error\n");
-        return false;
-    }
-    json verifyResponse = pre_state_read();
-    if(!verifyResponse["isExist"].get<bool>()){
-        err_printf("aid not exist\n");
-        return false;
-    }
-    if(!verifyResponse["result"].get<bool>()){
-        err_printf("aid password error\n");
-        return false;
-    }
-    return true;
+bool verifyUser(std::string aid, std::string password)
+{
+  // check if aid is valid
+  char **subArgv = (char **)malloc(sizeof(char *) * 4);
+  // put aidContractAddress into argv[0]
+  subArgv[0] = getDynamicString(aidContractAddress.c_str());
+  subArgv[1] = getDynamicString("verify");
+  subArgv[2] = getDynamicString(aid.c_str());
+  subArgv[3] = getDynamicString(password.c_str());
+  int ret = call_contract(aidContractAddress.c_str(), 4, subArgv);
+  removeDynamicStrings(subArgv);
+  if (ret != 0)
+  {
+    std::cerr << "aid contract exe error" << std::endl;
+    return false;
+  }
+  json verifyResponse = pre_state_read();
+  if (!verifyResponse["isExist"].get<bool>())
+  {
+    std::cerr << "aid not exist" << std::endl;
+    return false;
+  }
+  if (!verifyResponse["result"].get<bool>())
+  {
+    std::cerr << "aid password error" << std::endl;
+    return false;
+  }
+  return true;
 }
 
-extern "C" int contract_main(int argc, char **argv) {
+extern "C" int contract_main(int argc, char **argv)
+{
   // init state
-  if(!state_exist()){
+  if (!state_exist())
+  {
     json j = json::array();
     std::string coinName = argv[1];
     int count = atoi(argv[2]);
     std::string aid = argv[3];
-    for(int i=0;i<count;i++){
-        json tmp;
-        tmp["coinName"] = coinName;
-        tmp["id"] = i;
-        tmp["owner"] = aid;
-        j.push_back(tmp);
+    for (int i = 0; i < count; i++)
+    {
+      json tmp;
+      tmp["coinName"] = coinName;
+      tmp["id"] = i;
+      tmp["owner"] = aid;
+      j.push_back(tmp);
     }
     state_write(j);
     return 0;
   }
   // execute command
-  if (argc == 1) {
-      std::cerr << "argc error" << std::endl;
-      return 0;
+  if (argc == 1)
+  {
+    std::cerr << "argc error" << std::endl;
+    return 0;
   }
   std::string command = argv[1];
   auto eCommand = string2Command.find(command);
-  if (eCommand == string2Command.end()) {
-      std::cerr << "command error" << std::endl;
-      return 0;
+  if (eCommand == string2Command.end())
+  {
+    std::cerr << "command error" << std::endl;
+    return 0;
   }
   switch (eCommand->second)
   {
   case Command::totalSupply:
-    if(check_runtime_can_write_db()){
-        return 0;
+    if (check_runtime_can_write_db())
+    {
+      return 0;
     }
     state_write(state_read());
     break;
   case Command::balanceOf:
-    if(check_runtime_can_write_db()){
-        return 0;
+    if (check_runtime_can_write_db())
+    {
+      return 0;
     }
     {
-        std::string aid = argv[2];
-        json j = state_read();
-        json ownList = json::array();
-        for (auto &it : j) {
-            if (it["owner"] == aid) {
-                ownList.push_back(it);
-            }
+      std::string aid = argv[2];
+      json j = state_read();
+      json ownList = json::array();
+      for (auto &it : j)
+      {
+        if (it["owner"] == aid)
+        {
+          ownList.push_back(it);
         }
-        state_write(ownList);
+      }
+      state_write(ownList);
     }
     break;
   case Command::transfer:
-    if(!check_runtime_can_write_db()){
+    if (!check_runtime_can_write_db())
+    {
       return 0;
     }
     {
-        // get argv
-        std::string aid = argv[2];
-        std::string targetAid = argv[3];
-        int count = atoi(argv[4]);
-        std::string password = argv[5];
-        // check if aid is valid
-        if(!verifyUser(aid, password)){
-            return 0;
+      // get argv
+      std::string aid = argv[2];
+      std::string targetAid = argv[3];
+      int count = atoi(argv[4]);
+      std::string password = argv[5];
+      // check if aid is valid
+      if (!verifyUser(aid, password))
+      {
+        return 0;
+      }
+      // start execute transfer
+      json j = state_read();
+      json ownList = json::array();
+      for (auto &it : j)
+      {
+        if (it["owner"] == aid)
+        {
+          ownList.push_back(it);
         }
-        // start execute transfer
-        json j = state_read();
-        json ownList = json::array();
-        for (auto &it : j) {
-            if (it["owner"] == aid) {
-                ownList.push_back(it);
-            }
+      }
+      if (static_cast<int>(ownList.size()) < count)
+      {
+        std::cerr << "not enough token" << std::endl;
+        return 0;
+      }
+      for (int i = 0; i < count; i++)
+      {
+        for (auto &it : j)
+        {
+          if (it["owner"] == aid)
+          {
+            it["owner"] = targetAid;
+            break;
+          }
         }
-        if (static_cast<int>(ownList.size()) < count) {
-            err_printf("not enough token\n");
-            return 0;
-        }
-        for(int i=0;i<count;i++){
-            for(auto &it : j){
-                if(it["owner"] == aid){
-                    it["owner"] = targetAid;
-                    break;
-                }
-            }
-        }
-        state_write(j);
+      }
+      state_write(j);
     }
     break;
   case Command::allowance:
-    if(!check_runtime_can_write_db()){
+    if (!check_runtime_can_write_db())
+    {
       return 0;
     }
-    err_printf("allowance not implemented\n");
+    std::cerr << "allowance not implemented" << std::endl;
     break;
   case Command::approve:
-    if(!check_runtime_can_write_db()){
+    if (!check_runtime_can_write_db())
+    {
       return 0;
     }
-    err_printf("approve not implemented\n");
+    std::cerr << "approve not implemented" << std::endl;
     break;
   case Command::transferFrom:
-    if(!check_runtime_can_write_db()){
+    if (!check_runtime_can_write_db())
+    {
       return 0;
     }
-    err_printf("transferFrom not implemented\n");
+    std::cerr << "transferFrom not implemented" << std::endl;
     break;
   default:
     break;
