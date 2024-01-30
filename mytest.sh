1b17a12a216710131b6d77360cfe9938f913d0ab
leon123858 <a0970785699@gmail.com>
2024-01-30 08:22:20 +0000

    update GPoW for generate to address RPC

--- mytest.sh
+++ mytest.sh
@@ -1,17 +1,28 @@
+#!/bin/bash
+# Leon Lin for Ourchain
+
+mining() {
+    ./src/bitcoin-cli getnewaddress > address.txt
+    ./src/bitcoin-cli generatetoaddress $1 $(cat address.txt)
+    sleep 5
+    rm address.txt
+}
+
+deploycontract() {
+    ./src/bitcoin-cli deploycontract ~/Desktop/ourchain/sample.cpp > log.txt
+    # 使用 grep 和 awk 从 log.txt 文件中提取合同地址
+    contract_address=$(grep "contract address" log.txt | awk -F'"' '{print $4}')
+    rm log.txt
+    echo "$contract_address"
+}
+
 make -j8 && make install && ldconfig
 ./src/bitcoind --regtest --daemon -txindex
 sleep 5
-./src/bitcoin-cli generate 11
-sleep 5
-./src/bitcoin-cli deploycontract ~/Desktop/ourchain/sample.cpp > log.txt
-./src/bitcoin-cli generate 1
-# 使用 grep 和 awk 从 log.txt 文件中提取合同地址
-contract_address=$(grep "contract address" log.txt | awk -F'"' '{print $4}')
-rm log.txt
-# 输出提取的合同地址
-echo "Contract Address: $contract_address"
-# 使用合同地址调用合同
+mining 11
+contract_address=$(deploycontract)
+mining 1
 ./src/bitcoin-cli callcontract "$contract_address" ""
-./src/bitcoin-cli generate 2
+mining 2
 ./src/bitcoin-cli dumpcontractmessage "$contract_address" ""
 ./src/bitcoin-cli stop
